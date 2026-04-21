231e415d6a780cc7299a4442597fef1341214b66
Lakindu Jayathilaka <lakindu.20241937@iit.ac.lk>
2026-04-21 23:09:23 +0530

    chore: rename main application class and clean up services

---
+++ src/main/java/com/westminster/smartcampus/SmartCampusApplication.java
@@ -0,0 +1,8 @@
+// Author: 2151955/ 20241937 / Lakindu Jayathilaka
+
+package com.westminster.smartcampus;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+}
